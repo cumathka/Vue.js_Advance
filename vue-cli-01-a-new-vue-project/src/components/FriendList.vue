bc2bacff6797593442e48d3970be03ae2f456d9f
cumathka <cumakaya2531@gmail.com>
2024-04-26 12:10:11 +0200

    freund-list ok

---
+++ vue-cli-01-a-new-vue-project/src/components/FriendList.vue
@@ -0,0 +1,34 @@
+<template>
+    <section>
+        <ul>
+            <li v-for="friend in friends" :key="friend.id">
+                <p>{{ friend.name }}</p>
+                <button @click="toggleDetails(friend.id)">Show Details</button>
+                <div>
+                    <ul v-if="detailsAreVisible === friend.id">
+                        <li>{{ friend.phone }} </li>
+                        <li> {{ friend.email }}</li>
+                    </ul>
+                </div>
+            </li>
+        </ul>
+    </section>
+</template>
+
+<script>
+export default {
+    props: ['friends'],
+  data() {
+    return {
+        detailsAreVisible  : null,
+    }
+  },
+   methods :{
+    toggleDetails (id){
+        
+        this.detailsAreVisible = this.detailsAreVisible === id ? null : id ;    
+        console.log(this.detailsAreVisible);   
+    }
+   }
+}
+</script>
