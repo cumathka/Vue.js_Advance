4d6727cb74d0dd9156f04fa6f5b08fc6185dc0c9
cumathka <cumakaya2531@gmail.com>
2024-04-27 21:37:24 +0200

    list app ok

---
+++ cmp-communication-01-starting-setup/src/components/NewFriend.vue
@@ -0,0 +1,44 @@
+<template>
+  <form @submit.prevent="submitData">
+    <div>
+      <label>Name :</label>
+      <input type="text" v-model="enteredName" />
+    </div>
+    <div>
+      <label>Phone :</label>
+      <input type="tel" v-model="enteredPhone" />
+    </div>
+    <div>
+      <label>Email :</label>
+      <input type="email" v-model="enteredEmail" />
+    </div>
+    <div>
+      <button>Add new name to list!</button>
+    </div>
+  </form>
+</template>
+
+
+<script>
+export default {
+  emits: ["add-contact"],
+  data() {
+    return {
+      enteredName: "name",
+      enteredPhone: "phone",
+      enteredEmail: "email",
+    };
+  },
+
+  methods: {
+    submitData() {
+      this.$emit(
+        "add-contact",
+        this.enteredName,
+        this.enteredPhone,
+        this.enteredEmail
+      );
+    },
+  },
+};
+</script>
