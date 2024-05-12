e6ea2c1a4d0546532088bd8a58f2c0071f76f482
cumathka <cumakaya2531@gmail.com>
2024-05-12 18:01:53 +0200

    vue router ok

---
+++ routing-01-starting-setup/src/components/nav/NotFoundPage.vue
@@ -0,0 +1,6 @@
+<template>
+    <h2>
+       Page Not Found ! Maybe View Our.
+       <router-link to="/teams">Teams</router-link> 
+    </h2>
+</template>
---
+++ routing-01-starting-setup/src/main.js
@@ -0,0 +1,69 @@
+import { createApp } from 'vue';
+import { createRouter, createWebHistory } from 'vue-router';
+
+import App from './App.vue';
+import TeamsList from './components/teams/TeamsList.vue';
+import UsersList from './components/users/UsersList.vue';
+import TeamMembers from './components/teams/TeamMembers.vue';
+import NotFoundPage from './components/nav/NotFoundPage.vue';
+import TeamsFooter from './components/teams/TeamsFooter.vue';
+import UsersFooter from './components/users/UsersFooter.vue';
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    { path: '/', redirect: '/teams' },
+    {
+      path: '/teams',
+      name: 'teams',
+      components: { default: TeamsList, footer: TeamsFooter },
+      children: [
+        {
+          name: 'team-members',
+          path: ':teamId',
+          component: TeamMembers,
+          props: true,
+        },
+      ],
+    }, // our-domain.com/teams => TeamsList //, alias : '/'
+    { path: '/users', components: { default: UsersList, footer: UsersFooter },
+        beforeEnter(to,from,next){
+            console.log('users beforeEnter');
+            console.log(to,from);
+            next();
+        }
+    }, // our-domain.com/users => UsersList
+
+    { path: '/:notFound(.*)', component: NotFoundPage },
+  ],
+  linkActiveClass: 'active',
+  scrollBehavior(to, from, savedPosition) {
+    console.log(to, from, savedPosition);
+    if (savedPosition) {
+      return savedPosition;
+    }
+    return { left: 0, top: 0 };
+  },
+});
+
+router.beforeEach(function (to, from, next) {
+  console.log(to, from);
+  // if(to.name === 'team-members'){
+  //     next()
+  // }else {
+  //     next({name: 'team-members', params : {teamId : 't2'}});
+  // }
+  next();
+});
+
+router.afterEach(function(to,from){
+    //sending analytics data
+     console.log('global afterEach');
+    console.log(to,from);
+})
+
+const app = createApp(App);
+
+app.use(router);
+
+app.mount('#app');
